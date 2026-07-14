4dba414f303912be1f97c20b62309f3aa0298e90
Hasan Hazbi <showkathazbi99@gmail.com>
2026-07-14 11:34:59 +0600

    Update Chatbot.py
    Excluded "greetings" from dictionary. Used random module inside the loop for greetings and for error_handling.  Now, bot can use three different phrases to greet the user. 
    
    Added "tired" to the dictionary.

--- Chatbot.py
+++ Chatbot.py
@@ -9,24 +9,30 @@ class Robot:
 
 robot = Robot("Silica", "Version 0.0.0.1", "Incomplete" )
 print(f"Chatbot name : {robot.name}, {robot.model}, Model status: {robot.model_completeness}")
-print("Hello, I am silica. Your Pybot companion.")
-user = {"greetings": "Hello, how are you today?(Input 'help' for more information)", "sad": "You don't have to go through this alone, find someone who can help you IRL.", "happy": "Oh, enjoy!", "angry": "Calm down, tiger! XD", "bored": "Go do something fun!", "help": """For time : Type 'time'
+print("Hello, I am Silica. Your Pybot companion.")
+
+
+user = {"sad": "You don't have to go through this alone, find someone who can help you in real life.", "tired": "Take some rest then.", "happy": "Oh, enjoy!", "angry": "Calm down, tiger! XD", "bored": "Go do something fun!", "help": """For time : Type 'time'
 For quitting : Type 'quit'"""}
 user_input = ""
 while True:
     user_input = input("User : ")
     
 
-    if "hi" in user_input.lower() or "hello" in user_input.lower():
-        print(user["greetings"])
-    elif "sad" in user_input.lower():
+    if "hi" in user_input.lower() or "hello" in user_input.lower() or "hey" in user_input.lower():
+        greet_dict = ["Hey, nice to have you here! How are you feeling today?", "Hi, it's great to have you here! What's the mood?", "Hello user! How are you today?"]
+        bot_choice = rand.choice(greet_dict)
+        print(bot_choice)
+    elif "sad" in user_input.lower() or "depressed" in user_input.lower():
         print(user["sad"])
     elif "angry" in user_input.lower() or "mad" in user_input.lower():
         print(user["angry"])
-    elif "happy" in user_input.lower():
+    elif "happy" in user_input.lower() or "well" in user_input.lower() or "fine" in user_input.lower():
         print(user["happy"])
     elif "bored" in user_input.lower():
-        print(user[""])
+        print(user["bored"])
+    elif "tired" in user_input.lower() or "exhausted" in user_input.lower():
+        print(user["tired"])
     elif user_input.lower() == "help":
         print()
         print(user["help"])
@@ -38,7 +44,8 @@ while True:
         now = time.now()
         print(now)
     else:
-        input_handling = ["Sorry, I don't understand the input, please try again.", "Failed to process, please try again.", "Invalid input, please try again."]
-        choice = rand.choice(input_handling)
+        error_handling = ["Sorry, I don't understand the input, please try again.", "Failed to process, please try again.", "Invalid input, please try again."]
+        choice = rand.choice(error_handling)
         print(choice)
     
+
