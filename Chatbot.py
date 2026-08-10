3d3b912db232a351422f51cedbc2833a78b1c15c
Hasan Hazbi <showkathazbi99@gmail.com>
2026-08-10 17:42:26 +0600

    Update Chatbot.py
    Added custom naming for the user. Stored the mood value into a variable. 👍👍👍

--- Chatbot.py
+++ Chatbot.py
@@ -23,6 +23,7 @@ def typindicator():
 user = {"sad": "You don't have to go through this alone, find someone who can help you in real life.", "Any other": "If you need any other help, I am here.", "tired": "Take some rest then.", "happy": "Oh, enjoy!", "angry": "Calm down, tiger! XD", "bored": "Go do something fun!", "help": """For time : Type 'time'
 For quitting : Type 'quit'"""}
 User = input("What do you want me to call you : ")
+mood = None
 user_input = ""
 while True:
     user_input = input(f"{User} : ")
@@ -38,18 +39,23 @@ while True:
         print(user["Any other"])
     elif "sad" in user_input.lower() or "depressed" in user_input.lower():
         typindicator()
+        mood = "sad"
         print(user["sad"])
     elif "angry" in user_input.lower() or "mad" in user_input.lower():
         typindicator()
+        mood = "angry"
         print(user["angry"])
     elif "happy" in user_input.lower() or "well" in user_input.lower() or "fine" in user_input.lower():
         typindicator()
+        mood = "happy"
         print(user["happy"])
     elif "bored" in user_input.lower():
         typindicator()
+        mood = "bored"
         print(user["bored"])
     elif "tired" in user_input.lower() or "exhausted" in user_input.lower():
         typindicator()
+        mood = "tired"
         print(user["tired"])
     elif user_input.lower() == "help":
         print()
@@ -63,11 +69,22 @@ while True:
         now = datetime.now()
         typindicator()
         print(now)
+    elif "thank" in user_input.lower() or "thnx" in user_input.lower():
+        typindicator()
+        print("You're most welcome. If you need any other help, let me know.")
+    elif "mood" in user_input.lower():
+        if "mood" != None:
+            typindicator()
+            print(f"You're currently {mood}.")
+        else:
+            typindicator()
+            print("You haven't told me your mood yet. State your mood then, please.")
     else:
         error_handling = ["Sorry, I don't understand the input, please try again.", "Failed to process, please try again.", "Invalid input, please try again."]
         choice = rand.choice(error_handling)
         typindicator()
         print(choice)
     
+
 
 
